c8486de4b61e21572de4d67db45dd717bfdf1546
opauloh <paulo.scape@gmail.com>
2020-09-14 09:17:45 -0300

    App component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from './components/Title';
+
+export default function App() {
+  return (
+    <div>
+      <Title>React TS Github</Title>
+    </div>
+  );
+}
