d1cf80380162c95a1deb4a292d66634e7e35b165
opauloh <paulo.scape@gmail.com>
2020-09-14 09:59:22 -0300

    add Search component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Search from './components/Search';
 import Title from './components/Title';
 
 export default function App() {
   return (
     <div className="container">
       <Title>React TS Github</Title>
+      <Search onSubmit={() => ({})} />
     </div>
   );
 }
