6ff5dfbcd8b8f93ab6dd09489249b513ff00d00b
opauloh <paulo.scape@gmail.com>
2020-09-14 09:18:01 -0300

    application index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => <div>Hello</div>;
+import App from './App';
+import './global.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
