6295e237f2bd3a81cc43e870ae9cc9239550e7ff
opauloh <paulo.scape@gmail.com>
2020-09-14 09:58:41 -0300

    Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,37 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import styles from './Search.module.css';
+
+export default function Search({ onSubmit }) {
+  const [username, setUsername] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit(username);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username" className={styles.label}>
+        Github Username
+      </label>
+      <div className={styles.search}>
+        <input
+          type="text"
+          id="username"
+          className={styles.input}
+          placeholder="Type a Github username to fetch results"
+          autoComplete="off"
+          value={username}
+          onChange={handleChange}
+        />
+        <button className={styles.btn} type="submit" disabled={!username}>
+          SEARCH
+        </button>
+      </div>
+    </form>
+  );
+}
