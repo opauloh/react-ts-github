b44d91e0c2c85d23bbfda7b8dca8d797fc73946b
opauloh <paulo.scape@gmail.com>
2020-09-15 17:53:03 -0300

    add Result component

---
+++ src/components/Result.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useEffect, useReducer } from 'react';
+import { resultsReducer, resultsState } from '../reducers/resultReducer';
+import { getProfile } from '../utils/graphql';
+import Profile from './Profile';
+import styles from './Result.module.css';
+
+type Props = {
+  username: string;
+};
+
+const Result: FC<Props> = ({ username }) => {
+  const [{ error, profile, loading }, dispatch] = useReducer(
+    resultsReducer,
+    resultsState
+  );
+
+  useEffect(() => {
+    getProfile(username)
+      .then((profile) => dispatch({ type: 'success', profile }))
+      .catch(({ message }) => dispatch({ type: 'error', error: message }));
+  }, [username]);
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div className={styles.error}>{error}</div>;
+
+  return <div>{profile && <Profile profile={profile} />}</div>;
+};
+
+export default Result;
