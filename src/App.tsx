9266c393c4f7eafefc644de5ef4df8fe004c13b5
opauloh <paulo.scape@gmail.com>
2020-09-15 23:19:08 -0300

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Result from './components/Result';
 import Search from './components/Search';
 import Title from './components/Title';
 
 export default function App() {
+  const [username, setUsername] = useState<null | string>(null);
+
+  const onSubmit = (profile: string) => setUsername(profile);
+
   return (
     <div className="container">
       <Title>React TS Github</Title>
-      <Search onSubmit={() => ({})} />
+      <Search onSubmit={onSubmit} />
+      {username && <Result username={username} />}
     </div>
   );
 }
