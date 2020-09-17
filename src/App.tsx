c9482c52b00a0846e3d622a8e1c5db2716b68b21
opauloh <paulo.scape@gmail.com>
2020-09-17 01:06:00 -0300

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import React, { useState } from 'react';
+import React, { Dispatch, useReducer, useState } from 'react';
+import Repositories from './components/Repositories';
 import Result from './components/Result';
 import Search from './components/Search';
 import Title from './components/Title';
+import UserContext from './contexts/userContext';
+import {
+  ResultAction,
+  resultsReducer,
+  resultsState,
+  ResultState
+} from './reducers/resultReducer';
+
+export interface IUsername {
+  username: string;
+  valid: Boolean;
+}
 
 export default function App() {
-  const [username, setUsername] = useState<null | string>(null);
+  const [username, setUsername] = useState<string | null>(null);
 
-  const onSubmit = (profile: string) => setUsername(profile);
+  const [profile, dispatch]: [ResultState, Dispatch<ResultAction>] = useReducer(
+    resultsReducer,
+    resultsState
+  );
+
+  const onSubmit = (username: string) => {
+    dispatch({ type: 'reset' });
+    setUsername(username);
+  };
 
   return (
-    <div className="container">
-      <Title>React TS Github</Title>
-      <Search onSubmit={onSubmit} />
-      {username && <Result username={username} />}
-    </div>
+    <UserContext.Provider value={[profile, dispatch]}>
+      <div className="container">
+        <Title>React TS Github</Title>
+        <Search onSubmit={onSubmit} />
+        {username && <Result username={username} />}
+        <Repositories />
+      </div>
+    </UserContext.Provider>
   );
 }
