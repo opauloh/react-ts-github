5eb161c1caee813c2281fbe25345e3017862dc03
opauloh <paulo.scape@gmail.com>
2020-09-17 00:22:48 -0300

    upgrade resultReducer

--- src/reducers/resultReducer.ts
+++ src/reducers/resultReducer.ts
@@ -7,26 +7,29 @@ export const resultsState = {
   profile: null
 };
 
-type State = {
+export interface ResultState {
   error?: string | null;
   loading?: boolean;
   profile: IProfile | null;
-};
+}
 
-type Action =
+export type ResultAction =
+  | { type: 'reset' }
   | { type: 'loading' }
   | { type: 'error'; error: string }
   | { type: 'success'; profile: IProfile };
 
-export const resultsReducer: Reducer<State, Action> = (
-  state: State,
-  action: Action
+export const resultsReducer: Reducer<ResultState, ResultAction> = (
+  state: ResultState,
+  action: ResultAction
 ) => {
   switch (action.type) {
+    case 'reset':
+      return resultsState;
     case 'loading':
       return { ...state, loading: true };
     case 'error':
-      return { ...state, error: action.error, loading: false };
+      return { ...state, error: action.error, loading: false, profile: null };
     case 'success':
       return { ...state, profile: action.profile, error: null, loading: false };
   }
