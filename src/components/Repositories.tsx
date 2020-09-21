cc7ab7fe811254b2c23a07b355f13ccf6220f598
opauloh <paulo.scape@gmail.com>
2020-09-21 10:29:25 -0300

    redux responsibility Repositories component

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -1,11 +1,5 @@
-import React, { Dispatch, FC, useContext, useEffect, useReducer } from 'react';
-import { getRepositories } from '../api';
-import UserContext from '../contexts/userContext';
-import {
-  repositoriesReducer,
-  repositoriesState
-} from '../reducers/repositoriesReducer';
-import { ResultAction, ResultState } from '../reducers/resultReducer';
+import React, { FC } from 'react';
+import useRepositories from '../hooks/useRepositories';
 import Button from './Button';
 import Loading from './Loading';
 import styles from './Repositories.module.css';
@@ -13,60 +7,40 @@ import Repository from './RepositoryItem';
 import Title from './Title';
 
 const Repositories: FC = () => {
-  const [{ profile }]: [ResultState, Dispatch<ResultAction>] = useContext(
-    UserContext
-  );
-
-  const [
-    { error, repositories, loading, sorting, cursor },
-    dispatch
-  ] = useReducer(repositoriesReducer, repositoriesState);
-
-  useEffect(() => {
-    dispatch({ type: 'reset' });
-  }, [profile]);
-
-  useEffect(() => {
-    if (profile) {
-      dispatch({ type: 'loading' });
-      getRepositories(profile.login, sorting, cursor)
-        .then((repositories) => dispatch({ type: 'success', repositories }))
-        .catch(({ message }) => dispatch({ type: 'error', error: message }));
-    }
-  }, [profile, sorting, cursor]);
+  const {
+    profile,
+    loading,
+    repositories,
+    error,
+    sorting,
+    sort,
+    loadMore
+  } = useRepositories();
 
   if (!profile) return <></>;
   if (!repositories) return <>empty</>;
   if (error) return <>error</>;
 
+  const { totalCount } = profile.repositories;
+  const totalRepositories = repositories.length;
+
   return (
     <div className={styles.repositories}>
       <div className={styles.header}>
         <Title>
-          Showing {repositories.length} Repositories out of{' '}
-          {profile.repositories.totalCount}{' '}
+          Showing {totalRepositories} Repositories out of {totalCount}{' '}
         </Title>
         <div>
-          <Button onClick={() => dispatch({ type: 'sorting' })}>
-            Sorting name: {sorting}
-          </Button>
+          <Button onClick={sort}>Sorting name: {sorting}</Button>
         </div>
       </div>
-      {repositories.map((repository) => (
-        <Repository key={repository.node.id} repository={repository.node} />
+      {repositories.map(({ node }) => (
+        <Repository key={node.id} repository={node} />
       ))}
       <div>
         {loading && <Loading text="Loading Repositories" />}
-        {repositories.length < profile.repositories.totalCount && (
-          <Button
-            onClick={() =>
-              dispatch({
-                type: 'load_more',
-                cursor: repositories[repositories.length - 1].cursor
-              })
-            }>
-            Load More
-          </Button>
+        {totalRepositories < totalCount && (
+          <Button onClick={loadMore}>Load More</Button>
         )}
       </div>
     </div>
